06f5bebf3d4be2004aee654df44eef444a8055d9
RafasyaSubhan <rafasya.subhan@gmail.com>
2026-05-13 20:18:19 +0700

    Adding github folder for deployment

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Memberi tahu Next.js untuk mem-build aplikasi menjadi file HTML statis
+  output: "export",
+  
+  // Wajib untuk Static Export agar gambar tidak error
+  images: {
+    unoptimized: true,
+  },
+  
+  basePath: "/segaramakmur", 
 };
 
 export default nextConfig;
